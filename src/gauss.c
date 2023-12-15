9104a02ec8dfea9816a037503404bae5edcc8f79
KrzysztofG <krzys.gumi@gmail.com>
2023-12-15 18:05:09 +0100

    Dodanie eliminacji Gaussa
    Napisałem w pliku gauss.c funkcję eliminate, która przeprowadza eliminację Gaussa, także sprawdza czy podczas tego procesu nie dzielimy przez zero i czy macierz nie jest osobliwa.

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,35 @@
 #include "gauss.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+	
+	if (mat->r != mat->c){
+		printf("Macierz nie jest kwadratowa, nie mogę rozwiązać.");
+		return 1;
+	}
+	int i;	
+	for(i = 0; i < mat->r - 1 ; i++ ){ // pętla po przekątnej; gdy jesteśmy w ostatnim wierszu to kończymy dlatego r-1
+		if(mat->data[i][i] == 0){ // macierz osobliwa?
+			return 1;
+		}
+		for (int j = i + 1; j < mat->r; j++){  // pętla liczy dla każdego wiersza wsp
+			if(mat->data[j][i] == 0) { //dzielenie przez 0
+				return 1;	
+			} 
+			double wsp = mat->data[j][i] / mat-> data[i][i]; // wsp to współczynnik
+			for (int g = i; g < mat->r; g++ ){ // pętla odejmuje wektor;
+				mat->data[j][g] -= mat->data[i][g] * wsp; //odejmowanie wektora
+			}
+		}
+		
+	}
 
-		return 0;
-}
+	if(mat->data[i][i] == 0){
+                        return 1; // macierz osobliwa?
+        }
+
+	return 0;
+	
+} 
 
